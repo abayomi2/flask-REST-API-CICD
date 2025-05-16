f3a69a9950e311164888c2ded380c5160ba8da22
abayomi2 <oguntiloye101@gmail.com>
2025-05-16 19:21:46 +1000

    update fil

--- application/app.py
+++ application/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, DevOps World! Version 1.0'
+    return 'Hello, DevOps World! Version 2.0'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
